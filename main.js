8dc8328724e85a8ecb2be5d9c806cc27d3267e11
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-03-30 07:20:30 +0900

    16.診断機能の開発

--- main.js
+++ main.js
@@ -29,3 +29,14 @@ const wordgame = {
 };
 
 console.log(wordgame.evaluation("apple"));
+
+function showalphabet() {
+  const virtualKyboard = document.getElementById("virtualkeyboard");
+  const aCharCode = 'A'.charCodeAt(0);
+  for (let i = aCharCode; i < aCharCode + 26; i++) {
+    const letter = String.fromCharCode(i);
+    virtualKyboard.innerText += letter;
+  }
+}
+
+showalphabet();
