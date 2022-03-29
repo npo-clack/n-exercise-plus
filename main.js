ea8ec3baab8fe5324ea3596dd387bfb3c4c349c3
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-03-30 08:46:43 +0900

    18.ツイート機能の開発

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-let counter = 0;
-
-function execount() {
-  counter++;
-  return counter;
-}
-
-function showexecount(event) {
-  let inputKey;
-  if(event.key) {
-    inputKey = event.key;
-  } else {
-    inputKey = event.target.innerText;
-  }
-
-  const timeElement = document.getElementById("keyboardinput");
-  timeElement.innerText = inputKey;
-
-  execount(); //下記の判定のためにカウントする必要がある
-  // 書き方はいろいろ、isRedみたいなフラグを保持してもよい
-  if (counter % 2 == 0) {
-    timeElement.className = "red";
-  } else {
-    timeElement.className = "blue";
-  }
-}
-
-// setInterval(showexecount, 1000);
-document.onkeydown = showexecount;
-
 const wordgame = {
   question: "りんごの英単語は？",
   correct: "apple",
@@ -36,7 +6,60 @@ const wordgame = {
   }
 };
 
-console.log(wordgame.evaluation("apple"));
+const questionElement = document.getElementById("question");
+questionElement.innerText = wordgame.question;
+
+let inputKeys = "";
+
+function showexecount(event) {
+  let inputKey;
+  if (event.key) {
+    inputKey = event.key;
+  } else {
+    inputKey = event.target.innerText;
+  }
+
+  const resultEelement = document.getElementById("result");
+  resultEelement.className = "result-hidden";
+
+  //何も要素がないれば処理しない
+  if (!inputKey) {
+    return;
+  }
+  // enter, delete 以外の2文字以上の入力は使わない
+  if (inputKey != "Enter" && inputKey != "Delete" && inputKey.length >= 2) {
+    return;
+  }
+  // 数字も扱わない
+  if (!Number.isNaN(Number(inputKey))) {
+    return;
+  }
+
+  if (inputKey == "Enter") {
+    // 正誤判定
+    const resultEelement = document.getElementById("result");
+    resultEelement.className = "result";
+    if (wordgame.evaluation(inputKeys)) {
+      resultEelement.innerText = "正解";
+    } else {
+      resultEelement.innerText = "不正解";
+    }
+    return;
+  }
+
+  if (inputKey == "Delete") {
+    let deleteLast = inputKeys.substring(0, inputKeys.length - 1);
+    inputKeys = deleteLast;
+  } else {
+    inputKeys += inputKey;
+  }
+
+  const timeElement = document.getElementById("keyboardinput");
+  timeElement.innerText = inputKeys;
+}
+
+// setInterval(showexecount, 1000);
+document.onkeydown = showexecount;
 
 function showalphabet() {
   const virtualKyboard = document.getElementById("virtualkeyboard");
@@ -48,6 +71,16 @@ function showalphabet() {
     childButton.onclick = showexecount;
     virtualKyboard.appendChild(childButton);
   }
+
+  const enterButton = document.createElement("button");
+  enterButton.innerText = "Enter";
+  enterButton.onclick = showexecount;
+  virtualKyboard.appendChild(enterButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.onclick = showexecount;
+  virtualKyboard.appendChild(deleteButton);
 }
 
 showalphabet();
